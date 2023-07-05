46261f5b548449e0a4a988043548e38c465f3f47
Anik07Ghosh <120354990+Anik07Ghosh@users.noreply.github.com>
2023-07-05 13:30:20 +0530

    Add files via upload

---
+++ 11_dictionary.py
@@ -0,0 +1,3 @@
+d1 = {"anik":"old monk", "pratyush":"sutta",
+      "barshan":"ganja", "debashish":"flake"}
+print(d1["barshan"]
---
+++ htd.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+time.sleep(2)
+count=0
+while count<=500:
+    pyautogui.typewrite("VMROOOOOOOOOOOO")
+    pyautogui.press("enter")
+    count=count+1
